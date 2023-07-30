7722929c30d2f942c14228267c0a9d672276c579
Henry Goes Zanin <henrygzanin@gmail.com>
2023-07-30 19:38:20 -0300

    acessando dados dentro do json

---
+++ Dart/trabalhando_com_json.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+void main() {
+  Map<String, dynamic> dados = json.decode(dadosDoUsuario());
+  print(dados['nome']);
+  print(dados['endereco']['cidade']);
+  print(dados['cursos']);
+}
+
+String dadosDoUsuario() {
+  return """ 
+  {
+    "nome": "Henry",
+    "sobrenome": "Goes Zanin",
+    "idade": 21,
+    "casado": false,
+    "altura": 1.75,
+    "cursos": [
+      {
+        "nome": "Dart",
+        "dificuldade": "1"
+      },
+      {
+        "nome": "Flutter",
+        "dificuldade": 1
+      }
+    ],
+    "endereco": {
+      "cidade": "Campinas",
+      "pais": "Brasil",
+      "numero": 100
+    }
+  }
+  """;
+}
