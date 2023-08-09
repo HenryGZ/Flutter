a4a4c9f1f943134566a582027424f859ad527175
Henry Goes Zanin <henrygzanin@gmail.com>
2023-08-08 23:23:15 -0300

    alterações no primeiro app

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -17,28 +17,74 @@ class MyApp extends StatelessWidget {
   }
 }
 
+void decressimo() {
+  print('decressimo');
+}
+
+void acressimo() {
+  print('acressimo');
+}
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     // ignore: prefer_const_constructors
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: const Color.fromARGB(255, 255, 0, 0),
       // ignore: prefer_const_constructors
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
-          Text('Pode entrar!',
+        children: [
+          const Text('Contador de cliques',
               style: TextStyle(
                 fontSize: 28,
                 color: Color.fromARGB(255, 195, 195, 195),
                 fontWeight: FontWeight.w800,
               )),
-          Text(
-            '0',
-            style: TextStyle(fontSize: 100, color: Colors.white),
-          )
+
+          //const SizedBox(height: 30),
+          const Padding(
+            padding: EdgeInsets.all(30),
+            child: Text(
+              '0',
+              style: TextStyle(fontSize: 100, color: Colors.white),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: decressimo,
+                style: TextButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    backgroundColor: Colors.white,
+                    fixedSize: const Size(100, 50),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    padding: const EdgeInsets.all(15)),
+                child: const Text(
+                  'Menos',
+                  style: TextStyle(color: Colors.black, fontSize: 20),
+                ),
+              ),
+              const SizedBox(width: 30),
+              TextButton(
+                onPressed: acressimo,
+                style: TextButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    backgroundColor: Colors.white,
+                    fixedSize: const Size(100, 50),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    padding: const EdgeInsets.all(15)),
+                child: const Text(
+                  'Mais',
+                  style: TextStyle(color: Colors.black, fontSize: 20),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
