7acb82eacd934b66074fffbbdc8de2ce6759ebec
Henry Goes Zanin <henrygzanin@gmail.com>
2023-08-28 19:56:21 -0300

    inicio do projeto

---
+++ lista_de_tarefas/lib/pages/lista_de_tarefas.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class TodoListPage extends StatelessWidget {
+  TodoListPage({Key? key}) : super(key: key);
+
+  final TextEditingController emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              TextField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              ElevatedButton(onPressed: null, child: Text('Enviar'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void entrar() {
+    String text = emailController.text;
+    print(text);
+    emailController.clear();
+  }
+}
